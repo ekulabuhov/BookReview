70ee3b7a56be8f4f32cbc617f7d4a37bd0afe4ab
reemrd <jerichouni@gmail.com>
2014-01-26 15:58:17 +0200

    checking the commit option.vs2013

--- BookReview/Controllers/AddCommentController.cs
+++ BookReview/Controllers/AddCommentController.cs
@@ -19,6 +19,7 @@ namespace BookReview.Controllers
         // POST: /Reply/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        //hello moto
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Reply(int bukid, string comment)
