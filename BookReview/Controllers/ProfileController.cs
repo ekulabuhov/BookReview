6c3242e63b68d1fe1caa42f9a052c0aa0650fc97
reemrd <jerichouni@gmail.com>
2014-01-29 22:47:30 +0200

    Some additional checks implemented. Profiles are working as expected now.

--- BookReview/Controllers/ProfileController.cs
+++ BookReview/Controllers/ProfileController.cs
@@ -14,45 +14,51 @@ namespace BookReview.Controllers
 
         //
         // GET: /Profile/
-        public ActionResult Index(string UserProfile)
+        public ActionResult Index(string commentedBy)
         {
             //if no parameter passed, open current user profile
-            if (UserProfile == null)
+            if (commentedBy == null)
             {
-                var queryUserId = User.Identity.GetUserId();
-                var result = db.AspNetUsers.Find(queryUserId);
+                var queriedBy = User.Identity.GetUserId();
+                var result = db.AspNetUsers.Find(queriedBy);
                 return View(result);
             }  else
             {
-                var result = db.AspNetUsers.First(p => p.FullName == UserProfile);
+                var result = db.AspNetUsers.First(p => p.FullName == commentedBy);
+                //gotta make a FAKE queries check 
                 return View(result);
             }
-        } 
+        }
 
-        public ActionResult _FavBooks(string queryProfile)
+        public ActionResult _FavBooks(string queriedFavBooks)
         {
-            var queryUserId = User.Identity.GetUserId();
-            var blah = db.AspNetUsers.Find(queryUserId); 
+            //Залогиненый юзер
+            var loggedInUserGUID = User.Identity.GetUserId();
+            var loggedInUserDetails = db.AspNetUsers.Find(loggedInUserGUID);
+            //Искомый юзер
+            var queriedUserDetails = (db.AspNetUsers.Where(b => b.FullName == queriedFavBooks)).Single();
+            //var queriedUserGUID = queriedUserDetails.Id;
+      
+                
             var result = (from s in db.Books
                           from c in s.BookAspNetUsers
-                          where c.AspNetUserId == queryUserId
-                          select s).ToList();
-            //Check if the current user is the owner of that profile
-            if (queryProfile != null && queryProfile != blah.FullName)
-            {
-               foreach (var huj in result)
-                {
-                   //this is used as a flag to be passed to view. I decided to go for this approach because I prefer not to use Viewbag..Or because the voices in my head told me to do so...
-                    huj.BookAspNetUsers.Clear();
-                } 
-            }
+                          where c.AspNetUserId == queriedUserDetails.Id
+                          select s).ToList(); 
              
+                //Check if the current user is the owner of that profile
+                if (loggedInUserGUID == null || (queriedFavBooks != null && queriedFavBooks != loggedInUserDetails.FullName))
+                {
+                    foreach (var huj in result)
+                    {
+                        //this is used as a flag to be passed to view. I decided to go for this approach because I prefer not to use Viewbag..Or because the voices in my head told me to do so...
+                        huj.BookAspNetUsers.Clear();
+                    }
+                } 
                 return PartialView(result);
              
         }
 
-
-
+         
         public ActionResult AddToFavs(int id)
         {
             BookAspNetUsers BookUserJunction = new BookAspNetUsers();
@@ -66,8 +72,9 @@ namespace BookReview.Controllers
 
         public ActionResult RemoveFromFav(int id)
         {
-            BookAspNetUsers BookUserJunction = db.BookAspNetUsers.Single(b => b.BookId.Equals(id));
-            db.BookAspNetUsers.Remove(BookUserJunction);
+            var loggedInUser = User.Identity.GetUserId();  
+            BookAspNetUsers bookUserJunction = (db.BookAspNetUsers.Where(b => b.BookId == id && b.AspNetUserId == loggedInUser)).Single(); 
+            db.BookAspNetUsers.Remove(bookUserJunction);
             db.SaveChanges();
             return RedirectToAction("Index");
         }
