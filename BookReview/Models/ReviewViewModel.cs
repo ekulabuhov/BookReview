4b9514e02f4dc7e03c3ec6ccab3306bcaf4cf973
reemrd <jerichouni@gmail.com>
2014-02-01 02:15:19 +0200

    Little fix

---
+++ BookReview/Models/ReviewViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookReview.Models
+{
+    public class ReviewViewModel
+    {
+
+        public ICollection<Review> Reviews { get; set; } 
+        public int BookId { get; set; }
+        public string Text { get; set; }
+        public System.DateTime PostedOn { get; set; }
+        public string FullName { get; set; } 
+
+    }
+}
